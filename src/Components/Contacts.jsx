d95799948affc1263723f2ebdeba09d2c719873e
MAYANK <mayanklander.com>
2024-03-03 22:53:08 +0530

    final

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -59,205 +59,6 @@ const Contacts = () => {
 
     <div id='contact' className='h-[100vh] flex flex-col items-center justify-between w-full'>
 
-
-
-<div className='fixed w-full h-full text-[#fff] bg-[#00000073] z-30'>
-            <div class="socket  ">
-                <div class="gel center-gel">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c1 r1">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c2 r1">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c3 r1">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c4 r1">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c5 r1">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c6 r1">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-
-                <div class="gel c7 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-
-                <div class="gel c8 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c9 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c10 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c11 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c12 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c13 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c14 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c15 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c16 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c17 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c18 r2">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c19 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c20 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c21 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c22 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c23 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c24 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c25 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c26 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c28 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c29 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c30 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c31 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c32 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c33 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c34 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c35 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c36 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-                <div class="gel c37 r3">
-                    <div class="hex-brick h1"></div>
-                    <div class="hex-brick h2"></div>
-                    <div class="hex-brick h3"></div>
-                </div>
-
-            </div>
-
-
-        </div>
-
-
-      
       <div className="ss:w-fit w-[80%]" >
         <h2 className='sm:text-[90px] text-center text-[60px]'>Contacts.</h2>
         <motion.div
@@ -273,25 +74,20 @@ const Contacts = () => {
 
 
 
-            <h4 className='text-centertext-[18px] pb-2'>For any help or project collaboration</h4>
-     
+          <h4 className='text-centertext-[18px] pb-2'>For any help or project collaboration</h4>
 
-              <form ref={ref} onSubmit={submitHandler} className=" w-full flex flex-col">
-                <div className="flex flex-col p-4">
-                  <label className=''>Your Email</label>
-                  <input value={data.email} onChange={valueupdate} required="" className='bg-[#131312] outline-none focus:border-[#fefeff] p-2 border-[#747373] text-[#fefeff] rounded-md border-2 ' name="email" id="email" type="email" />
-                </div>
-                <div className="flex flex-col p-4">
-                  <label >Message</label>
-                  <textarea value={data.message} name='message' onChange={valueupdate} className='bg-[#131312] outline-none focus:border-[#fefeff] p-2 border-[#747373] text-[#fefeff] rounded-md border-2 ' required='' cols="30" rows="3" id="textarea" />
-                </div>
-                <button type="submit" className="py-2 px-4 mx-auto text-[#131312] bg-[#fefeff] rounded-lg font-poppins hover:bg-[#909090] font-semibold">Submit</button>
-              </form>
-       
 
-            
-          
-
+          <form ref={ref} onSubmit={submitHandler} className=" w-full flex flex-col">
+            <div className="flex flex-col p-4">
+              <label className=''>Your Email</label>
+              <input value={data.email} onChange={valueupdate} required="" className='bg-[#131312] outline-none focus:border-[#fefeff] p-2 border-[#747373] text-[#fefeff] rounded-md border-2 ' name="email" id="email" type="email" />
+            </div>
+            <div className="flex flex-col p-4">
+              <label >Message</label>
+              <textarea value={data.message} name='message' onChange={valueupdate} className='bg-[#131312] outline-none focus:border-[#fefeff] p-2 border-[#747373] text-[#fefeff] rounded-md border-2 ' required='' cols="30" rows="3" id="textarea" />
+            </div>
+            <button type="submit" className="py-2 px-4 mx-auto text-[#131312] bg-[#fefeff] rounded-lg font-poppins hover:bg-[#909090] font-semibold">Submit</button>
+          </form>
 
         </motion.div>
       </div>
@@ -308,8 +104,6 @@ const Contacts = () => {
             <a href="https://www.linkedin.com/in/mayank-kashyap-20114a24b/" className="w-[50px] z-50 m-2 rounded-full p-3 h-[50px] linkedin"
             ><FaLinkedin className='w-full h-full' /></a>
           </div>
-
-
         </div>
 
         <div>
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import TextTransition, { presets } from 'react-text-transition';
 
-const TEXTS = [' मयंक कश्यप ', 'Mayank Kashyap'];
 
 const Hero = () => {
   const [mousePosition, setMousePosition] = useState({
@@ -36,18 +34,10 @@ const Hero = () => {
       y: mousePosition.y - 16,
     },
   };
-  // useScript('')
 
 
-  const [index, setIndex] = React.useState(0);
 
-  useEffect(() => {
-    const intervalId = setInterval(
-      () => setIndex((index) => index + 1),
-      3000, // every 3 seconds
-    );
-    return () => clearTimeout(intervalId);
-  }, []);
+
   return (
     <div className='pointer-events-none'>
       <div className='ss:block h-0 hidden'>
@@ -59,11 +49,10 @@ const Hero = () => {
       </div>
 
       <div id='hero' className='w-full h-[100vh] justify-center z-10 flex flex-col items-center'>
-        <h1 className=' overflow-visible font-caveat px-3 font-bold ss:text-[130px] text-[60px] herotext text-center '>
+        <h1 className=' overflow-visible font-caveat px-3 font-bold ss:text-[130px] text-[90px] herotext text-center '>
 
           {/* मयंक कश्यप */}
           Mayank Kashyap
-          {/* <TextTransition delay={1} className='font-mont' springConfig={presets.gentle}>{TEXTS[index % TEXTS.length]}</TextTransition> */}
 
         </h1>
         
